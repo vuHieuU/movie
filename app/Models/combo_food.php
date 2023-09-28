1b1ff615b1b9c753472bf859d8a0191d0475d8e8
Hieu Vu <vuhieu13102003@gmail.com>
2023-09-28 10:17:54 +0700

    database

--- app/Models/combo_food.php
+++ app/Models/combo_food.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class combofood_food extends Model
+class combo_food extends Model
 {
     use HasFactory;
 }
