db68604c49a4b8665d3a38e37920aa78be1c9110
Your NameNguyenVanMinh <minh12335678899@gmail.com>
2023-10-10 12:44:12 +0700

    upfate

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('/', [App\Http\Controllers\client\homeController::class, 'index']);
 Route::get('/contact', [App\Http\Controllers\client\ContactController::class, 'index']);
 Route::get('/detail_blog', [App\Http\Controllers\client\Detail_blogController::class, 'index']);
 Route::get('/weeklyshowtime', [App\Http\Controllers\client\WeeklyShowtimeController::class, 'index']);
+Route::get('/weeklyshowtime/{id}', [App\Http\Controllers\client\WeeklyShowtimeController::class, 'show'])->name("weeklyshowtime");
+
+
 
 
 // Route::get('/detail_film/{id}', [App\Http\Controllers\client\Detail_filmController::class, 'index']);
