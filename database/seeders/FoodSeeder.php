7af35224bc915fc1c5265f672f8a924b2d476f3e
datndph24577 <93152474+datndph24577@users.noreply.github.com>
2023-10-13 18:41:16 +0700

    update

---
+++ database/seeders/FoodSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class FoodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('food')->insert([
+            [
+                'name' => 'Combo 1',
+                'thumb' => 'https://files.betacorp.vn/files/media/images/2023/06/15/sweet-combo-154545-150623-48.png',
+                'price' => 69000,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Combo 2',
+                'thumb' => 'https://files.betacorp.vn/files/media/images/2023/06/15/beta-combo-154428-150623-83.png',
+                'price' => 99000,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Combo 3',
+                'thumb' => 'https://files.betacorp.vn/files/media/combopackage/2023/07/27/combo-see-me-08-083746-270723-92.png',
+                'price' => 129999,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],     
+        ]);
+    }
+}
