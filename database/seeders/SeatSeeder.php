b81b2d83884216fcb8a3d958c8cc1dacce5313ce
Son <son452018@gmail.com>
2023-11-14 03:55:56 +0700

    son updade

--- database/seeders/SeatSeeder.php
+++ database/seeders/SeatSeeder.php
@@ -13,100 +13,17 @@ class SeatSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table("seats")->insert([
-            ["seat_number"=>"A1" , "typeSeat_id"=>1],
-            ["seat_number"=>"A2" , "typeSeat_id"=>1],
-            ["seat_number"=>"A3" , "typeSeat_id"=>1],
-            ["seat_number"=>"A4" , "typeSeat_id"=>1],
-            ["seat_number"=>"A5" , "typeSeat_id"=>1],
-            ["seat_number"=>"A6" , "typeSeat_id"=>1],
-            ["seat_number"=>"A7" , "typeSeat_id"=>1],
-            ["seat_number"=>"A8" , "typeSeat_id"=>1],
-            ["seat_number"=>"A9" , "typeSeat_id"=>1],
-            ["seat_number"=>"A10" , "typeSeat_id"=>1],
-            ["seat_number"=>"A11" , "typeSeat_id"=>1],
-            ["seat_number"=>"A12" , "typeSeat_id"=>1],
-            ["seat_number"=>"A13" , "typeSeat_id"=>1],
-            ["seat_number"=>"A14" , "typeSeat_id"=>1],
-            ["seat_number"=>"A15" , "typeSeat_id"=>1],
-            ["seat_number"=>"A16" , "typeSeat_id"=>1],
-            ["seat_number"=>"A17" , "typeSeat_id"=>1],
-            ["seat_number"=>"A18" , "typeSeat_id"=>1],
-            ["seat_number"=>"A19" , "typeSeat_id"=>1],
-            ["seat_number"=>"A20" , "typeSeat_id"=>1],
+        $seatLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+        $maxNumber = 12;
+        $seats = [];
 
+        foreach ($seatLetters as $letter) {
+            for ($i = 1; $i <= $maxNumber; $i++) {
+                $seatNumber = $letter . $i;
+                $seats[] = ['seat_number' => $seatNumber, 'typeSeat_id' => 1];
+            }
+        }
 
-
-            ["seat_number"=>"B1" , "typeSeat_id"=>2],
-            ["seat_number"=>"B2" , "typeSeat_id"=>2],
-            ["seat_number"=>"B3" , "typeSeat_id"=>2],
-            ["seat_number"=>"B4" , "typeSeat_id"=>2],
-            ["seat_number"=>"B5" , "typeSeat_id"=>2],
-            ["seat_number"=>"B6" , "typeSeat_id"=>2],
-            ["seat_number"=>"B7" , "typeSeat_id"=>2],
-            ["seat_number"=>"B8" , "typeSeat_id"=>2],
-            ["seat_number"=>"B9" , "typeSeat_id"=>2],
-            ["seat_number"=>"B10" , "typeSeat_id"=>2],
-            ["seat_number"=>"B11" , "typeSeat_id"=>2],
-            ["seat_number"=>"B12" , "typeSeat_id"=>2],
-            ["seat_number"=>"B13" , "typeSeat_id"=>2],
-            ["seat_number"=>"B14" , "typeSeat_id"=>2],
-            ["seat_number"=>"B15" , "typeSeat_id"=>2],
-            ["seat_number"=>"B16" , "typeSeat_id"=>2],
-            ["seat_number"=>"B17" , "typeSeat_id"=>2],
-            ["seat_number"=>"B18" , "typeSeat_id"=>2],
-            ["seat_number"=>"B19" , "typeSeat_id"=>2],
-            ["seat_number"=>"B20" , "typeSeat_id"=>2],
-
-            ["seat_number"=>"C1" , "typeSeat_id"=>3],
-            ["seat_number"=>"C2" , "typeSeat_id"=>3],
-            ["seat_number"=>"C3" , "typeSeat_id"=>3],
-            ["seat_number"=>"C4" , "typeSeat_id"=>3],
-            ["seat_number"=>"C5" , "typeSeat_id"=>3],
-            ["seat_number"=>"C6" , "typeSeat_id"=>3],
-            ["seat_number"=>"C7" , "typeSeat_id"=>3],
-            ["seat_number"=>"C8" , "typeSeat_id"=>3],
-            ["seat_number"=>"C9" , "typeSeat_id"=>3],
-            ["seat_number"=>"C10" , "typeSeat_id"=>3],
-            ["seat_number"=>"C11" , "typeSeat_id"=>3],
-            ["seat_number"=>"C12" , "typeSeat_id"=>3],
-            ["seat_number"=>"C13" , "typeSeat_id"=>3],
-            ["seat_number"=>"C14" , "typeSeat_id"=>3],
-            ["seat_number"=>"C15" , "typeSeat_id"=>3],
-            ["seat_number"=>"C16" , "typeSeat_id"=>3],
-            ["seat_number"=>"C17" , "typeSeat_id"=>3],
-            ["seat_number"=>"C18" , "typeSeat_id"=>3],
-            ["seat_number"=>"C19" , "typeSeat_id"=>3],
-            ["seat_number"=>"C20" , "typeSeat_id"=>3],
-            ["seat_number"=>"C21" , "typeSeat_id"=>3],
-            ["seat_number"=>"C22" , "typeSeat_id"=>3],
-            ["seat_number"=>"C23" , "typeSeat_id"=>3],
-            ["seat_number"=>"C24" , "typeSeat_id"=>3],
-
-            ["seat_number"=>"D1" , "typeSeat_id"=>4],
-            ["seat_number"=>"D2" , "typeSeat_id"=>4],
-            ["seat_number"=>"D3" , "typeSeat_id"=>4],
-            ["seat_number"=>"D4" , "typeSeat_id"=>4],
-            ["seat_number"=>"D5" , "typeSeat_id"=>4],
-            ["seat_number"=>"D6" , "typeSeat_id"=>4],
-            ["seat_number"=>"D7" , "typeSeat_id"=>4],
-            ["seat_number"=>"D8" , "typeSeat_id"=>4],
-            ["seat_number"=>"D9" , "typeSeat_id"=>4],
-            ["seat_number"=>"D10" , "typeSeat_id"=>4],
-            ["seat_number"=>"D11" , "typeSeat_id"=>4],
-            ["seat_number"=>"D12" , "typeSeat_id"=>4],
-            ["seat_number"=>"D13" , "typeSeat_id"=>4],
-            ["seat_number"=>"D14" , "typeSeat_id"=>4],
-            ["seat_number"=>"D15" , "typeSeat_id"=>4],
-            ["seat_number"=>"D16" , "typeSeat_id"=>4],
-            ["seat_number"=>"D17" , "typeSeat_id"=>4],
-            ["seat_number"=>"D18" , "typeSeat_id"=>4],
-            ["seat_number"=>"D19" , "typeSeat_id"=>4],
-            ["seat_number"=>"D20" , "typeSeat_id"=>4],
-            ["seat_number"=>"D21" , "typeSeat_id"=>4],
-             ["seat_number"=>"D22" , "typeSeat_id"=>4],
-             ["seat_number"=>"D23" , "typeSeat_id"=>4],
-             ["seat_number"=>"D24" , "typeSeat_id"=>4],
-                    ]);
+        DB::table('seats')->insertOrIgnore($seats);
     }
 }
