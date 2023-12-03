772dbda967f2e46c1a702a9c4edf87001f2f7c69
TriChua18 <146014654+ShopAccTri@users.noreply.github.com>
2023-12-03 13:33:11 +0700

    Tri-Update Blog

--- resources/views/client/DetailBlog.blade.php
+++ resources/views/client/DetailBlog.blade.php
@@ -21,29 +21,19 @@
 	<div class="container">
 		<div class="row">
 			
-			<div class="col-md-8">
+			<div class="col-md-12">
 				<div class="page-content">
 					
 <article id="post-55" class="post-55 post type-post status-publish format-standard has-post-thumbnail category-news">
 	<div class="entry-top ">
 		<div class="entry-thumb">
-			{{-- <img style="width: 30%;" src="{{ asset('/storage/images/' . $detailblog->thumbnail) }}" class="attachment-full size-full wp-post-image" alt=""   />		</div> --}}
 	</div>
 	<div class="entry-bottom ">
 		<!-- Post Meta -->
 		<div class="entry-meta">
-			{{-- <div class="entry-date">
-				<span class="d">28</span>
-				<span class="m">Apr</span>
-				<span>{{$detailblog->created_at->format("d-m-Y")}}</span>
-			</div> --}}
-			{{-- <div class="entry-comment">
-				<i class="fa fa-comments" aria-hidden="true"></i>
-				0
-			</div> --}}
 		</div>
 		<div class="entry-left">
-			<h1 class="entry-title p-name" itemprop="name headline"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/2022/04/28/win-a-wizarding-world-holiday-with-fantastic/" rel="bookmark" class="u-url url" itemprop="url">{{$detailblog->title}}   </a></h1>
+			<h1 class="entry-title p-name" itemprop="name headline">{{$detailblog->title}}</h1>
 			<div>
 				<span>Ngày đăng: {{$detailblog->created_at->format("d/m/Y")}}</span>
 			</div>
@@ -57,7 +47,7 @@
 					</div>
 		<div class="clearfix"></div>
 	</div>
-	<!-- Post Tag -->
+	{{-- <!-- Post Tag -->
 	<div class="entry-info">
 		<div class="tag-box">
 			<span class="top-corner"></span>
@@ -66,7 +56,7 @@
 			<label>Share:</label>
 					</div>
 		<div class="clearfix"></div>
-	</div>
+	</div> --}}
 
 	<!-- Related Post -->
 	<div class="entry-related">
@@ -105,30 +95,10 @@
 					<div class="clearfix"></div>
 	</div>
 
-	
-<div class="amy-comment-form">
-		<div id="respond" class="comment-respond">
-		<h3 id="reply-title" class="comment-reply-title amy-title">Write a comment <small><a rel="nofollow" id="cancel-comment-reply-link" href="/movie/demo/elementor-single-cinema/2022/04/28/win-a-wizarding-world-holiday-with-fantastic/#respond" style="display:none;">Cancel reply</a></small></h3><form action="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-comments-post.php" method="post" id="commentform" class="comment-form" ><p class="comment-notes"><span id="email-notes">Your email address will not be published.</span> <span class="required-field-message" aria-hidden="true">Required fields are marked <span class="required" aria-hidden="true">*</span></span></p><p class="comment-form-comment"><label for="comment">Comment <span class="required">*</span></label><textarea name="comment" id="comment" cols="45" rows="8" max-length="65525" aria-required="true" required="required"></textarea><div class="row"><div class="col-md-4"><p class="comment-form-author"><label for="author">Name <span class="required">*</span></label><input type="text" value="" id="author" name="author" size="30" maxlength="245" aria-required="true" required="required" /></p></div>
-<div class="col-md-4"><p class="comment-form-email"><label for="email">Email <span class="required">*</span></label><input type="email" value="" id="email" name="email" size="30" maxlength="100" aria-describedby="email-notes" aria-required="true" required="required"/></p></div>
-<div class="col-md-4"><p class="comment-form-url"><label for="url">Website</label><input type="url" value="" id="url" name="url" size="30" maxlength="200" /></p></div></div>
-<p class="comment-form-cookies-consent"><input id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes" /> <label for="wp-comment-cookies-consent">Save my name, email, and website in this browser for the next time I comment.</label></p>
-<p class="form-submit"><input name="submit" type="submit" id="submit" class="submit" value="Post Comment" /> <input type='hidden' name='comment_post_ID' value='55' id='comment_post_ID' />
-<input type='hidden' name='comment_parent' id='comment_parent' value='0' />
-</p></form>	</div><!-- #respond -->
-	</div>
-
 </article>
 				</div>
 			</div>
 
-			<div class="col-md-4 amy-sidebar-clear"><div class="amy-page-sidebar amy-sidebar-right">
-<aside id="sidebar">
-	<div class="amy-widget widget_block widget_media_image">
-<figure class="wp-block-image size-full"><img loading="lazy" width="350" height="272" src="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_62.png" alt="" class="wp-image-254" srcset="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_62.png 350w, http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_62-300x233.png 300w" sizes="(max-width: 350px) 100vw, 350px" /></figure>
-<div class="clear"></div></div><div class="amy-widget amy-widget-list"><div class="amy-widget amy-widget-list list-movie "><h4 class="amy-title amy-widget-title">Top Movie</h4><div class="entry-item"><div class="entry-thumb"><img class="" src="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_20-118x159_c.jpg" alt="Kubo and the Two Strings"/></div><div class="entry-content"><h2 class="entry-title"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/movie/kubo-and-the-two-strings/">Kubo and the Two Strings</a></h2><div><span class="duration"><i class="fa fa-clock-o"></i>02 hours 00 minutes</span></div><div class="genre"><span><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/genre/cartoon/">Cartoon</a>, <a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/genre/comic/">Comic</a></span></div></div><div class="clearfix"></div></div><div class="entry-item"><div class="entry-thumb"><img class="" src="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_19-118x159_c.jpg" alt="The Hurricane Heist"/></div><div class="entry-content"><h2 class="entry-title"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/movie/the-hurricane-heist/">The Hurricane Heist</a></h2><div><span class="duration"><i class="fa fa-clock-o"></i>01 hours 30 minutes</span></div><div class="genre"><span><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/genre/comic/">Comic</a>, <a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/genre/magic/">Magic</a></span></div></div><div class="clearfix"></div></div><div class="entry-item"><div class="entry-thumb"><img class="" src="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_17-118x159_c.jpg" alt="Jumanji: Welcome to the Jungle"/></div><div class="entry-content"><h2 class="entry-title"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/movie/jumanji-welcome-to-the-jungle/">Jumanji: Welcome to the Jungle</a></h2><div><span class="duration"><i class="fa fa-clock-o"></i>02 hours 30 minutes</span></div><div class="genre"><span><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/genre/cartoon/">Cartoon</a>, <a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/genre/sci-fi/">Sci-fi</a></span></div></div><div class="clearfix"></div></div><div class="entry-item"><div class="entry-thumb"><img class="" src="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_18-118x159_c.jpg" alt="Death Wish"/></div><div class="entry-content"><h2 class="entry-title"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/movie/death-wish/">Death Wish</a></h2><div><span class="duration"><i class="fa fa-clock-o"></i>01 hours 00 minutes</span></div><div class="genre"><span><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/genre/cartoon/">Cartoon</a>, <a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/genre/comic/">Comic</a></span></div></div><div class="clearfix"></div></div></div><div class="clear"></div></div><div class="amy-widget amy-widget-list"><div class="amy-widget amy-widget-list list-post "><h4 class="amy-title amy-widget-title">Recent Posts</h4><div class="entry-item"><div class="entry-thumb"><img src="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_59-115x85_c.jpg" /></div><div class="entry-content"><h2 class="entry-title"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/2022/04/28/six-book-to-film-adaptations-to-get-excited-about-this-autumn/">Six book-to-film adaptations to get excited about this autumn</a></h2><div class="entry-meta"><span class="entry-date">April 28, 2022</span><span> / </span><span class="entry-comment">0 Comments</span></div></div><div class="clearfix"></div></div><div class="entry-item"><div class="entry-thumb"><img src="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_57-115x85_c.jpg" /></div><div class="entry-content"><h2 class="entry-title"><a href="http://demo.amytheme.com/movie/demo/elementor-single-cinema/2022/04/28/the-beatles-eight-days-a-week-the-touring/">The Beatles: Eight Days a Week – The Touring</a></h2><div class="entry-meta"><span class="entry-date">April 28, 2022</span><span> / </span><span class="entry-comment">0 Comments</span></div></div><div class="clearfix"></div></div></div><div class="clear"></div></div><div class="amy-widget widget_block widget_media_image">
-<figure class="wp-block-image size-full"><img loading="lazy" width="350" height="290" src="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_63.png" alt="" class="wp-image-255" srcset="http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_63.png 350w, http://demo.amytheme.com/movie/demo/elementor-single-cinema/wp-content/uploads/sites/2/2022/05/img_63-300x249.png 300w" sizes="(max-width: 350px) 100vw, 350px" /></figure>
-<div class="clear"></div></div></aside>
-</div></div>
 		</div>
 	</div>
 </section>
